3d9c6b94639bdda3c415038703ad30259ff1a3df
mennystar0-cloud <mennystar0@gmail.com>
2025-09-04 16:28:40 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
+// Simple no-op SW para permitir instalación y futuros caches
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+
+// Network-first: deja pasar siempre; aquí podrías agregar caché si lo necesitas
+self.addEventListener('fetch', () => {});
 
